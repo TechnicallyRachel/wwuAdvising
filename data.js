059793e8e3070303afe7c4b47408eb97a200bf12
Rachel Giles <giles.datasci@gmail.com>
2019-09-02 14:15:29 -0700

    indexing slides

---
+++ data.js
@@ -0,0 +1,72 @@
+const data = {
+    "slides": [
+    {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 0,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+    {
+        "_id": "593e9297ec4cca9c56bf61af",
+        "index": 1,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 2,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 3,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 4,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 5,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 6,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 7,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 8,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 9,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 10,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 11,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 0,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
+        {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 13,
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg"
+    },
